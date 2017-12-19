f9744985e2428c1eff91c6cdb9c8804fbd80b33d
Tania Roberts <robertstania@outlook.com>
2017-12-19 11:20:04 -0700

    Fixed Styling to Match Splash Pg Mockup

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
-$(document).ready(function() {
-    $.get("https://lit-fortress-6467.herokuapp.com/object ", function(data) {
-      console.log(data);
-      for (var i = 0; i < 3; i++) {
-        console.log(data.results[i]);
-        console.log(data.results[i].cover_art);
-        console.log(data.results[i].title);
-
-        var j = Math.floor(Math.random() * (i + 1));
-        data.results[i] = data.results[j];
-
+$(document).ready(function () {
+  $.get('https://lit-fortress-6467.herokuapp.com/object', function (data) {
+        console.log("data: ", data.results)
+    for (let i = 0; i < 3; i++) {
+      var j = Math.floor(Math.random() * (i + 1));
+      if(data.results[i] = data.results[j]) {
         var img = $('<img/>');
-        let title= $("<h1></h1>");
-
-        img.attr("src",  "./images/" + data.results[j].cover_art);
-        console.log("/Users/taniaroberts/Documents/Galvanize/Projects/Playlist/playlist/images/" + data.results[j].cover_art);
-
-        title.text(data.results[j].title);
-
-        $("#content").append(img);
-        $("#content").append(title);
-
-        $("#content").append(img);
-        $("#content").append(title);
-
-        $("#content").append(img);
-        $("#content").append(title);
+        img.attr("src", "./assets/images/" + data.results[j].cover_art);
+        $("#cdList").append(img)[j];
       }
-    })
-  //})
+    }
+  })
 })
